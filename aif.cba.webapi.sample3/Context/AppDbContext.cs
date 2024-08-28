b17ba73f9cf3617d72d2ce7cf527e45f5298645a
Pratham <net.pratham@gmail.com>
2024-08-28 14:42:39 +0530

    Add files via upload

---
+++ aif.cba.webapi.sample3/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DomainEntityModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("CRUDDB")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+    }
+}
---
+++ aif.cba.webapi.sample3/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using DomainEntityModel;
+using Infrastructure.Context;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET api/Employee
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET api/Employee/5
+        [ResponseType(typeof(Employee))]
+        public HttpResponseMessage GetEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                var message = string.Format("Employee with id = {0} not found", id);
+                HttpError err = new HttpError(message);
+                return Request.CreateResponse(HttpStatusCode.NotFound, err);                
+            }
+            
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+
+        // PUT api/Employee/5
+        public IHttpActionResult PutEmployee(int id, Employee employee)
+        {
+
+            if (id != employee.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Employee
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            var duplicateName = db.Employees.Where(b=>b.Name == employee.Name).SingleOrDefault();
+
+            if (duplicateName != null)
+            {
+                var msg = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(string.Format("Duplicate employee name found. ")),
+                    ReasonPhrase = "Duplicate"
+                };
+                throw new HttpResponseException(msg);
+            }
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.EmployeeID }, employee);
+        }
+
+        // DELETE api/Employee/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
